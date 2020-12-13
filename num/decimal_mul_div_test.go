5d0613972d5495d58adb66b24e363ab791b2da05
Drake (Vũ Long) <vu.long@moneyforward.vn>
2020-12-13 23:45:40 +0700

    Move the decimal package to root path

--- num/decimal_mul_div_test.go
+++ num/decimal_mul_div_test.go
@@ -1,36 +1,34 @@
-package num_test
+package num
 
 import (
 	"testing"
-
-	"github.com/lovung/gomath/num"
 )
 
 func Benchmark_2(b *testing.B) {
-	fee, _ := num.NewDecimalFromString("0.0295")
-	feeMin, _ := num.NewDecimalFromString("4")
-	feeAmt, _ := num.NewDecimalFromString("2.95")
+	fee, _ := NewDecimalFromString("0.0295")
+	feeMin, _ := NewDecimalFromString("4")
+	feeAmt, _ := NewDecimalFromString("2.95")
 	for n := 0; n < b.N; n++ {
 		feeMin.Div(feeAmt).Mul(fee)
 	}
 }
 
 func Benchmark_3(b *testing.B) {
-	fee, _ := num.NewDecimalFromString("0.0295")
-	feeMin, _ := num.NewDecimalFromString("4")
-	feeAmt, _ := num.NewDecimalFromString("2.95")
+	fee, _ := NewDecimalFromString("0.0295")
+	feeMin, _ := NewDecimalFromString("4")
+	feeAmt, _ := NewDecimalFromString("2.95")
 	for n := 0; n < b.N; n++ {
 		feeMin.Mul(fee.Div(feeAmt))
 	}
 }
 
 func Benchmark_4(b *testing.B) {
-	fee, _ := num.NewDecimalFromString("0.0295")
-	feeMin, _ := num.NewDecimalFromString("4")
-	feeAmt, _ := num.NewDecimalFromString("2.95")
-	feeFrac := num.NewFractionFromDecimal(fee)
-	feeMinFrac := num.NewFractionFromDecimal(feeMin)
-	feeAmtFrac := num.NewFractionFromDecimal(feeAmt)
+	fee, _ := NewDecimalFromString("0.0295")
+	feeMin, _ := NewDecimalFromString("4")
+	feeAmt, _ := NewDecimalFromString("2.95")
+	feeFrac := NewFractionFromDecimal(fee)
+	feeMinFrac := NewFractionFromDecimal(feeMin)
+	feeAmtFrac := NewFractionFromDecimal(feeAmt)
 	for n := 0; n < b.N; n++ {
 		feeMinFrac.Div(feeAmtFrac).Mul(feeFrac).Decimal()
 	}
