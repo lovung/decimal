76dc06f3892aed11c3b1ebc736c479e8dea757c1
Vũ Long <vulong1602@gmail.com>
2020-12-28 18:42:38 +0700

    Add reduce function

--- decimal.go
+++ decimal.go
@@ -21,74 +21,6 @@ type BigDecimal struct {
 	strCache string
 }
 
-// New returns the new BigDecimal from another BigDecimal
-func New(ref BigDecimal) BigDecimal {
-	ref.ensureInitialized()
-	return BigDecimal{
-		value:       new(big.Int).Set(ref.value),
-		scale:       ref.scale,
-		numerator:   ref.numerator,
-		denominator: ref.denominator,
-	}
-}
-
-// NewBigDecimal returns a new fixed-point big decimal.
-// Decimal = value * 10 ^ (-scale)
-//
-// Example: 0.19  ---> value = 19, scale = 2
-func NewBigDecimal(value int64, scale int32) BigDecimal {
-	return BigDecimal{
-		value: big.NewInt(value),
-		scale: scale,
-	}
-}
-
-// NewBigDecimalFromInt64 returns a new fixed-point big decimal with int64
-func NewBigDecimalFromInt64(value int64) BigDecimal {
-	return BigDecimal{
-		value: big.NewInt(value),
-	}
-}
-
-// NewBigDecimalFromInt returns a new fixed-point big decimal with int
-func NewBigDecimalFromInt(value int) BigDecimal {
-	return BigDecimal{
-		value: big.NewInt(int64(value)),
-	}
-}
-
-// NewBigDecimalFromBigInt returns a new fixed-point big decimal with big.Int and scale
-func NewBigDecimalFromBigInt(value *big.Int, scale int32) BigDecimal {
-	return BigDecimal{
-		value: new(big.Int).Set(value),
-		scale: scale,
-	}
-}
-
-// NewBigDecimalFromString returns a new BigDecimal from a string.
-// Alway want to have the zero-nearest positive-scale
-// Support:
-//		- Sign: NewBigDecimalFromString("-1")
-//				---> value: -1, scale: 0
-//		- Dot: NewBigDecimalFromString(".123")
-//				---> value: 123, scale: 3
-//		- E/e: NewBigDecimalFromString("1e9") / NewBigDecimalFromString(1E9)
-//				---> value: 1, scale: -9
-//		- Trim trailing zero: NewBigDecimalFromString("1.2300000")
-//				---> value: 123, scale: 2
-//		- Repeating/recurring decimal:
-//			NewBigDecimalFromString("0.(3)")
-//				---> value: 0, scale: 0, numerator: 1, denominator: 3
-//			NewBigDecimalFromString("1.23(3)")
-//				---> value: 12, scale: 1, numerator: 1, denominator: 3
-//			NewBigDecimalFromString("1.(428571)")
-//				---> value: 1, scale: 0, numerator: 3, denominator: 7
-//				-x-> value: 0, scale: -1, numerator: 1, denominator: 7
-func NewBigDecimalFromString(value string) (BigDecimal, error) {
-	// TODO: implement
-	return BigDecimal{}, nil
-}
-
 func (d *BigDecimal) ensureInitialized() {
 	if d.value == nil {
 		d.value = new(big.Int)
@@ -120,6 +52,22 @@ func (d *BigDecimal) optimize() {
 	if d.numerator == 0 {
 		d.denominator = 0
 	}
+}
+
+func (d *BigDecimal) reduce() {
+	if d.denominator == 0 {
+		if d.numerator != 0 {
+			panic("denominator is zero but numerator not")
+		}
+		return
+	}
+	if d.numerator == 0 {
+		d.denominator = 0
+		return
+	}
+	gcdOfND := gcd(d.denominator, d.numerator)
+	d.denominator /= gcdOfND
+	d.numerator /= gcdOfND
 }
 
 // rescale helps to change the scale value but keep the real decimal value.
--- decimal_test.go
+++ decimal_test.go
@@ -100,16 +100,90 @@ func TestBigDecimal_rescale(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			bd := BigDecimal{
+			d := BigDecimal{
 				value:       tt.fields.value,
 				scale:       tt.fields.scale,
 				numerator:   tt.fields.numerator,
 				denominator: tt.fields.denominator,
 				strCache:    tt.fields.strCache,
 			}
-			if got := bd.rescale(tt.scale); !reflect.DeepEqual(got, tt.want) {
+			if got := d.rescale(tt.scale); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("BigDecimal.rescale() = %v, want %v", got, tt.want)
 			}
 		})
 	}
+}
+
+func TestBigDecimal_reduce(t *testing.T) {
+	type fields struct {
+		value       *big.Int
+		scale       int32
+		numerator   uint64
+		denominator uint64
+		strCache    string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *BigDecimal
+	}{
+		{
+			fields: fields{
+				new(big.Int).SetInt64(1),
+				0, 0, 0, "",
+			},
+			want: &BigDecimal{
+				new(big.Int).SetInt64(1),
+				0, 0, 0, "",
+			},
+		},
+		{
+			fields: fields{
+				new(big.Int).SetInt64(1),
+				0, 0, 3, "",
+			},
+			want: &BigDecimal{
+				new(big.Int).SetInt64(1),
+				0, 0, 0, "",
+			},
+		},
+		{
+			fields: fields{
+				new(big.Int).SetInt64(1),
+				0, 9, 27, "",
+			},
+			want: &BigDecimal{
+				new(big.Int).SetInt64(1),
+				0, 1, 3, "",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &BigDecimal{
+				value:       tt.fields.value,
+				scale:       tt.fields.scale,
+				numerator:   tt.fields.numerator,
+				denominator: tt.fields.denominator,
+				strCache:    tt.fields.strCache,
+			}
+			d.reduce()
+			if !reflect.DeepEqual(d, tt.want) {
+				t.Errorf("BigDecimal.rescale() = %v, want %v", d, tt.want)
+			}
+		})
+	}
+	shouldPanic(t, func() {
+		d := BigDecimal{
+			new(big.Int).SetInt64(1),
+			0, 1, 0, "",
+		}
+		d.reduce()
+	})
+}
+
+func shouldPanic(t *testing.T, f func()) {
+	defer func() { recover() }()
+	f()
+	t.Errorf("should have panicked")
 }
